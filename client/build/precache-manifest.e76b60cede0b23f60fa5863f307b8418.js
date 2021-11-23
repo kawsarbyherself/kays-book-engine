5078d9e82664d6ae4d71076db6e725f15a44bb71
kawsarbyherself <kawsar_shafi@yahoo.com>
2021-11-23 13:55:52 -0800

    testing

---
+++ client/build/precache-manifest.e76b60cede0b23f60fa5863f307b8418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66bef0d098ab4b63c8d69ea3381eb0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5f7e55cc071255e2f5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b85ed62f7b3cefbb94c",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "2c5f7e55cc071255e2f5",
+    "url": "/static/js/2.6adaa7e7.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.6adaa7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b85ed62f7b3cefbb94c",
+    "url": "/static/js/main.280313f8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
